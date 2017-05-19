2b90b982497d0a29b6baa75c8eb6cdd7b34622a4
jahow <jahowz@163.com>
2017-05-19 22:10:32 +0800

    'BaiduCrowler_1.0'

---
+++ crawler.py
@@ -0,0 +1,47 @@
+import urllib2,urllib,httplib
+import argparse
+from bs4 import BeautifulSoup
+
+def GetHtml(url):
+	headers = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'} 
+	request = urllib2.Request(url=url,headers=headers)
+	return urllib2.urlopen(request).read()
+
+class BaiduAPI:
+	def __init__(self,keyword):
+		self.url = 'http://www.baidu.com/s?'+urllib.urlencode({'wd':keyword})
+	def GetUrl(self,pn):
+		return self.url+"&pn=%s"%(pn)
+
+class GoogleAPI:
+	def __init__(self,keyword):
+		self.url = 'http://www.google.com.hk/'
+
+def crawler(keyword,page):
+	api = BaiduAPI(keyword)
+#	api = GoogleAPI(keyword)
+	for pn in range(page):
+		url = api.GetUrl(str(pn)+'0')
+		html = GetHtml(url)
+		soup = BeautifulSoup(html,"html.parser")
+		result = soup.find_all('a',class_="c-showurl")
+		targetUrl = []
+		for i in result:
+			try:
+				print i['href']
+				targetUrl.append(urllib2.urlopen(i['href']).geturl())
+			except BaseException:
+				pass
+	print '\n'.join(targetUrl)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-s','--keyword',type=str)
+	parser.add_argument('-p','--page',type=int)
+	args = parser.parse_args()
+	if args.keyword:
+		if not args.page:
+			args.page = 1
+		crawler(args.keyword,args.page)
+	else:
+		print "input the -s 'keyword'"
